6f5f78ab1cd71c91df7079c24360cfe2ba78cb08
Evandro Jardini <eajardini@gmail.com>
2023-06-14 13:32:03 -0300

    Adicionado script de database

--- database/queries.js
+++ database/queries.js
@@ -1,5 +1,14 @@
 // create database escolaerp;
 
+// CREATE TABLE users (
+//   ID SERIAL PRIMARY KEY,
+//   name VARCHAR(30),
+//   email VARCHAR(30)
+// );
+
+// INSERT INTO users (name, email)
+//   VALUES ('Jerry', 'jerry@example.com'), ('George', 'george@example.com');
+
 
 const Pool = require('pg').Pool
 const pool = new Pool({
